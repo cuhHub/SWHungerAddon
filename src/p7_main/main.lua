36b6aeb5cc1e7d7ceeffc44c0090db1353a1199e
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-02-09 08:12:32 +0000

    add more fish + adjust "hungerDecreaseTimer" config property default value

--- src/p7_main/main.lua
+++ src/p7_main/main.lua
@@ -19,7 +19,7 @@ g_savedata.userAddonConfig = {
     -- do not split "property.blabla" over multiple lines. stormworks parses the text literally so it will not work and crash your game
     showUI = property.checkbox("UI - Show hunger level", true),
     startingHunger = property.slider("Hunger - Starting hunger level", 10.0, 100.0, 1.0, 80.0),
-    hungerDecreaseTimer = property.slider("Hunger - Time between periods of losing hunger (seconds)", 60.0, 400.0, 1.0, 130.0),
+    hungerDecreaseTimer = property.slider("Hunger - Time between periods of losing hunger (seconds)", 60.0, 400.0, 1.0, 155.0),
     hungerDecreaseAmount = property.slider("Hunger - The amount of hunger lost per hunger loss period", 1.0, 20.0, 1.0, 3.0),
 }
 
@@ -55,14 +55,14 @@ hungerLibrary.consumables.registerConsumable(
     "Anchovie",
     82,
     18,
-    3
+    3.9
 )
 
 hungerLibrary.consumables.registerConsumable(
     "Anglerfish",
     83,
     17,
-    2
+    2.2
 )
 
 hungerLibrary.consumables.registerConsumable(
@@ -76,28 +76,56 @@ hungerLibrary.consumables.registerConsumable(
     "Mackerel",
     100,
     13,
-    1
+    1.5
 )
 
 hungerLibrary.consumables.registerConsumable(
     "Salmon",
     109,
     17,
-    3
+    3.4
 )
 
 hungerLibrary.consumables.registerConsumable(
     "Sardine",
     110,
     14,
-    2
+    2.7
 )
 
 hungerLibrary.consumables.registerConsumable(
     "Trout",
     121,
     12,
-    4
+    4.5
+)
+
+hungerLibrary.consumables.registerConsumable(
+    "Viperfish",
+    123,
+    14,
+    2.8
+)
+
+hungerLibrary.consumables.registerConsumable(
+    "Snapper",
+    117,
+    -6,
+    5.5
+)
+
+hungerLibrary.consumables.registerConsumable(
+    "Piranha",
+    114,
+    -10,
+    7.2
+)
+
+hungerLibrary.consumables.registerConsumable(
+    "Tuna",
+    124,
+    16,
+    5.7
 )
 
 -- // Library Initialization
