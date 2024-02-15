88e001a4c79ce165d6ae778fcf1c401050f8971b
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-02-15 19:15:31 +0000

    fix invalid args

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-py combine.py --directory "src" --destination "src/script.lua" --allow_file_extension ".lua" --ignore_path "src/intellisense" --ignore_path "src/p1_framework/intellisense.lua"
+py combine.py --directory "src" --destination "src/script.lua" --allow_file_extension ".lua" --ignore_path "src/p1_framework/intellisense.lua" "src/intellisense"
 py sync.py -sf "src" -d "%appdata%/Stormworks/data/missions/SWHungerAddon"
